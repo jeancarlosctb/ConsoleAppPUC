06ce547970b8179638d940120e2b0fcddac9f296
jean <jean@grafftecnologia.com.br>
2024-08-18 22:46:35 -0300

    Mostrando Nome

--- ConsoleAppPUC/Program.cs
+++ ConsoleAppPUC/Program.cs
@@ -7,8 +7,16 @@ namespace ConsoleAppPUC // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
-            Console.WriteLine("\n Informe seu Nome:");
-            string texto = Console.ReadLine();
+            
+            string name = string.Empty;
+
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("\n Informe seu Nome:");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Bem vindo, {name}");
         }
     }
 }
